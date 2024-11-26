b62065fc582c7988037b99d2721525eabcc555d0
Cristian <cristian971107@hotmail.com>
2024-11-25 18:46:40 -0600

    mi tercer commit

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import streamlit as st
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
+scatter_button = st.button('Construir gráfico de dispersión')
 
 if hist_button: # al hacer clic en el botón
     # escribir un mensaje
@@ -14,3 +15,15 @@ if hist_button: # al hacer clic en el botón
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+    hist_button = st.button('Construir histograma') # crear un botón
+
+if scatter_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    
+    # crear un histograma
+    fig_1 = px.scatter(car_data, x="odometer", y="price")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig_1, use_container_width=True)
